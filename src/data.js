dba0e806b9e0b110837557b37ab5bacde43a0eda
hash-07 <63651724+hash-07@users.noreply.github.com>
2020-12-29 16:35:18 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,108 @@
+import whatsapp from "./whatsapp.png";
+import budgetCal from "./budgetCal.png";
+import website from "./website.png";
+import IncExp from "./IncExp.png";
+import insta from "./insta.png";
+import covid19 from './covid19.png'
+import toDoList from './toDoList.png'
+import recipes from './recipes.png'
+import keepNotes from './keepNotes.png'
+
+
+ const data = [
+ {
+  id: 1,
+  title: "WhatsApp Clone",
+  category: "breakfast",
+  img: whatsapp,
+  desc: "WhatsApp Clone with REACT JS, Material UI, React Hooks",
+  linkk: "https://whatsapp-clone-da347.web.app/",
+ },
+
+ {
+  id: 2,
+  title: "Bugdet Calculator",
+  category: "breakfast",
+  img: budgetCal,
+  desc: "Keeps the track of your Budget and returns the total spending",
+  linkk: "https://hash-07.github.io/BudgetCalc/",
+ },
+
+ {
+  id: 3,
+  title: "Courses Site",
+  category: "launch",
+  img: website,
+  desc: "Provides free courses which are paid on other websites",
+  linkk: "https://hash-07.github.io/website/",
+ },
+
+ {
+  id: 4,
+  title: "Income Expense Calculator",
+  category: "dinner",
+  img: IncExp,
+  desc:
+   "Keeps track of your Income and Expenses separately and returns the available balance",
+  linkk: "https://hash-07.github.io/website/",
+ },
+
+ {
+  id: 5,
+  title: "Instagram Clone",
+  category: "dinner",
+  img: insta,
+  desc:
+   "a full stack, INSTAGRAM CLONE using React, Firebase & Material-UI.",
+  linkk: "https://hash-07.github.io/instaClone/",
+ },
+
+ {
+  id: 6,
+  title: "Corona Virus tracker",
+  category: "dinner",
+  img: covid19,
+  desc:
+   "Its provides you data from all over the world about Covid-19 cases country wise, that are Infected, Recovered and Deaths ",
+  linkk: "https://hash-07.github.io/instaClone/",
+ },
+
+
+ {
+  id: 7,
+  title: "To Do list",
+  category: "dinner",
+  img: toDoList,
+  desc:
+   "A simple To-Do-List",
+  linkk: "https://hash-07.github.io/ToDoList/",
+ },
+
+ 
+ {
+  id: 8,
+  title: "Recipes",
+  category: "dinner",
+  img: recipes,
+  desc:
+   "Search your favourite recipe and checkout the ingredients",
+  linkk: "https://hash-07.github.io/Recipes/",
+ },
+
+ 
+ 
+ {
+  id: 9,
+  title: "Notes",
+  category: "dinner",
+  img: keepNotes,
+  desc:
+   "App for keeping notes as a reminder",
+  linkk: "https://hash-07.github.io/Recipes/",
+ },
+
+
+ 
+];
+
+export default data;
